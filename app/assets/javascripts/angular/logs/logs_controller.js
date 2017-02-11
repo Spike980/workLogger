1e630785fc811a302befe457e9261f6c23a7be53
Anand Mallick <anandmallick980@gmail.com>
2017-02-12 02:26:15 +0530

    select box for project perfected and edit project_name setup

--- app/assets/javascripts/angular/logs/logs_controller.js
+++ app/assets/javascripts/angular/logs/logs_controller.js
@@ -15,6 +15,7 @@
     var user_id = $scope.currentUserService.getCurrentUser().user_id
     $http.get("api/users/" + user_id + "/projects.json").success(function(data) {
      vm.projects = data;
+     console.log(vm.projects);
     });
 
     vm.rangeDescriptor = 'all';
@@ -23,6 +24,7 @@
       var now = new Date();
       vm.newLog = {
         'start_time': now,
+        'project_id': vm.projects[0].id
       }
     };
 
@@ -48,6 +50,10 @@
 
     vm.update = function () {
       logResource.update(vm.clonedLog);
+      var project = vm.projects.find(function(project) {
+        return vm.clonedLog.project_id == project.id;
+      });
+      vm.clonedLog.project.project_name = project.project_name;
       vm.logs.splice(vm.logs.indexOf(vm.editedLogMarker), 1, vm.clonedLog);
       vm.editedLogMarker = null;
       vm.clonedLog = null;
